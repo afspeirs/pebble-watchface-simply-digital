3f5dc924cac95b9307de71fea51227b95c3999d0
afspeirs <afspeirs@users.noreply.github.com>
2016-12-14 23:05:51 +0000

    Added more low battery options for testing

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -125,10 +125,30 @@ module.exports = [
 						"label": "40%",
 						"value": "40"
 					},
-// 					{ 
-// 						"label": "Always",
-// 						"value": "100" 
-// 					}
+					{
+						"label": "50%",
+						"value": "50"
+					},
+					{
+						"label": "60%",
+						"value": "60"
+					},
+					{
+						"label": "70%",
+						"value": "70"
+					},
+					{
+						"label": "80%",
+						"value": "80"
+					},
+					{
+						"label": "90%",
+						"value": "90"
+					},
+					{ 
+						"label": "Always",
+						"value": "100" 
+					}
 				]
 			},
 		]
